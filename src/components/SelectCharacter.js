25c90319ee385dcc7a0dc942354d34fb53e4982a
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-11-05 22:09:43 -0500

    Dando css al select

--- src/components/SelectCharacter.js
+++ src/components/SelectCharacter.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 export const SelectCharacter = ({ infoCharacters, selectCharacterQuote }) => {
     return (
-        <select name='names' onChange={selectCharacterQuote}>
-            <option value={-1}>-Select-Name-</option>
+        <select
+            className='select__container'
+            name='names'
+            onChange={selectCharacterQuote}
+        >
+            <option className='select__option' value={-1}>
+                --Select-Name--
+            </option>
             {infoCharacters.map(({ name }, index) => (
-                <option key={index} value={name}>
+                <option
+                    className='select__option--sons'
+                    key={index}
+                    value={name}
+                >
                     {name}
                 </option>
             ))}
